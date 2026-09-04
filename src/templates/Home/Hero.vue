76e9b22810d68ab5fa0d41e3ca442e7ea840b2c7
DEV-BR0 <lazizbekxoljigitov@gmail.com>
2026-09-04 13:37:49 +0500

    learn

--- src/templates/Home/Hero.vue
+++ src/templates/Home/Hero.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="lg:flex-row flex flex-col-reverse gap-[20px] h-6/12  mt-[-100px]">
+  <div class="lg:flex-row flex flex-col-reverse gap-[20px] h-6/12 mt-[-100px]">
     <div class="flex grow flex-col gap-[40px]">
       <div class="flex items-center gap-[5px]">
         <div class="bg-amber-300 h-[1px] w-[50px]"></div>
         <p class="font-[inner] text-white">Welcome to Pages</p>
       </div>
       <div class="flex flex-col gap-[30px]">
-        <h1 class="text-white lg:text-[80px] md:text-[40px] sm:text-[30px] text-[27px] font-[innes]">
+        <h1
+          class="text-white lg:text-[80px] md:text-[40px] sm:text-[30px] text-[27px] font-[innes]"
+        >
           Books are uniquely portable magic
         </h1>
         <p className="text-white lg:text-[18px] text-[15px] line-clamp-3 max-w-md">
-          There are many variations of passages of Lorem Ipsum available, but
-          the majority have suffered alteration in some form.
+          There are many variations of passages of Lorem Ipsum available, but the majority have
+          suffered alteration in some form.
         </p>
       </div>
       <div class="flex gap-[10px] items-center">
@@ -27,21 +29,21 @@
         </button>
       </div>
       <div class="flex gap-[20px] flex-wrap">
-        <div class="flex gap-[20px] grow text-white  ">
+        <div class="flex gap-[20px] grow text-white">
           <div class="w-[16px] h-[16px] bg-[#FFCA42] rounded-[50%] mt-[8px]"></div>
           <div class="flex flex-col gap-[10px]">
             <p class="font-[innes] text-[21px]">Length:</p>
             <p>10 Hours</p>
           </div>
         </div>
-        <div class="flex gap-[20px] text-white  grow">
+        <div class="flex gap-[20px] text-white grow">
           <div class="w-[16px] h-[16px] bg-[#FFCA42] rounded-[50%] mt-[8px]"></div>
           <div class="flex flex-col gap-[10px]">
             <p class="font-[innes] text-[21px]">Ratings:</p>
             <p>4.5/5 (305 ratings)</p>
           </div>
         </div>
-        <div class="flex gap-[20px] text-white  grow">
+        <div class="flex gap-[20px] text-white grow">
           <div class="w-[16px] h-[16px] bg-[#FFCA42] rounded-[50%] mt-[8px]"></div>
           <div class="flex flex-col gap-[10px]">
             <p class="font-[innes] text-[21px]">Pages:</p>
--- src/templates/Home/Learn.vue
+++ src/templates/Home/Learn.vue
@@ -1,9 +1,58 @@
 <template>
   <div data-aos="fade-up" class="contenr flex flex-col gap-[20px]">
-    <div class="flex flex-col w-full  items-center gap-[10px]">
+    <div class="flex flex-col w-full items-center gap-[10px]">
       <p class="text-[35px] font-[innes] font-bold text-[#1B3764]">What will you learn?</p>
       <div class="h-[2px] bg-[#FFCA42] w-[200px]"></div>
     </div>
-    <div class="bchsdbs">Loading...</div>
+    <div class="flex relative">
+      <div class="flex flex-wrap gap-[20px] ">
+        <div class="flex flex-col gap-[20px] bg-[#F4F8FF] w-[290px] p-[20px]">
+          <p
+            class="head bg-[#FFCA42] text-[25px] text-[#1B3764] font-[innes] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center font-bold"
+          >
+            01
+          </p>
+          <p class="text-[24px] line-clamp-4 max-w-[210px] font-[innes]">
+            Use HDFS & Map Reduce for storing & analyzing data at scale.
+          </p>
+        </div>
+        <div class="flex flex-col gap-[20px] w-[290px] bg-[#F4F8FF] p-[20px]">
+          <p
+            class="head bg-[#FFCA42] text-[25px] text-[#1B3764] font-[innes] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center font-bold"
+          >
+            02
+          </p>
+          <p class="text-[24px] line-clamp-4 max-w-[210px] font-[innes]">
+            Use HDFS & Map Reduce for storing & analyzing data at scale.
+          </p>
+        </div>
+        <div class="flex flex-col gap-[20px] bg-[#F4F8FF] w-[290px] p-[20px]">
+          <p
+            class="head bg-[#FFCA42] text-[25px] text-[#1B3764] font-[innes] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center font-bold"
+          >
+            03
+          </p>
+          <p class="text-[24px] line-clamp-4 max-w-[210px] font-[innes]">
+            Use HDFS & Map Reduce for storing & analyzing data at scale.
+          </p>
+        </div>
+        <div class="flex flex-col gap-[20px] bg-[#F4F8FF] w-[290px] p-[20px]">
+          <p
+            class="head bg-[#FFCA42] text-[25px] text-[#1B3764] font-[innes] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center font-bold"
+          >
+            04
+          </p>
+          <p class="text-[24px] line-clamp-4 max-w-[210px] font-[innes]">
+            Use HDFS & Map Reduce for storing & analyzing data at scale.
+          </p>
+        </div>
+      </div>
+      <div class="relative group sm:h-[540px]  w-[50%]">
+        <img src="/images/right-two.jpg" alt="right-two-Photo" class="w-[110%]]" />
+        <div
+          class="w-[105%] border border-[15px] border-amber-400 h-[100%] -z-10 top-0 absolute group-hover:w-[400px] group-hover:h-[400px] transition-all duration-200"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
